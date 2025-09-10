156d8ddfdcdb8cc6b0544d140d0672a3a129bbeb
Thanuja-Bathala <thanuthanuja1511@gmail.com>
2025-09-10 12:05:59 +0530

    upadate dsa section with responsive design

---
+++ script.js
@@ -0,0 +1,8 @@
+<script>
+    const menuToggle = document.querySelector(".menu-toggle");
+    const navLinks = document.querySelector("nav ul");
+
+    menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
+    });
+</script>
